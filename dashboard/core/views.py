b0a9d6fa56b410ea6d3ef2fddf3a234158ede15c
Carlos Moreno <omorenodovale@gmail.com>
2021-03-13 15:29:06 -0300

    chore: Add network metrics

--- dashboard/core/views.py
+++ dashboard/core/views.py
@@ -7,6 +7,7 @@ def home(request):
     disk = requests.get(config("API") + 'disk_space._')
     load = requests.get(config("API") + 'system.load')
     ram = requests.get(config("API") + 'system.ram')
+    net = requests.get(config("API") + 'net.eth0')
     context = {
         "disk_label": ['Disponível', 'Usado', 'Reservado para o root'],
         "disk_data": disk.json()['data'][0][1:],
@@ -14,5 +15,7 @@ def home(request):
         "load_data": load.json()['data'][0][1:],
         "ram_labels": ['Mem. livre', 'Mem. usada', 'Mem. cacheada', 'Buffers'],
         "ram_data": ram.json()['data'][0][1:],
+        "recebido": net.json()['data'][0][1],
+        "enviado": net.json()['data'][0][2] * -1,
     }
     return render(request, 'index.html', context)
